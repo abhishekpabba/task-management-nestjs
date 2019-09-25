134e92056ad8111ce465e699375e872b1104f277
Abhishek Pabba <pabba@Abhisheks-MBP.attlocal.net>
2019-09-24 20:25:01 -0500

    authentication using jwt and passport

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,10 +3,14 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TaskRepository]),
+    PassportModule,
+    AuthModule,
   ],
   controllers: [TasksController],
   providers: [TasksService],
